45a2db606d4afca217ff168fe086e9c18f2b1382
Игорь <1>
2018-12-10 12:48:04 +0300

    commit2

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,25 +1,50 @@
 $(document).ready(function() {
-    var leftUIEl = $(".carousel_arrow_left");
-    var rightUIEl = $(".carousel_arrow_right");
-    var elementsList = $(".carousel_list");
+    var leftUIEl = $(".carousel-arrow-left");
+    var rightUIEl = $(".carousel-arrow-right");
+    var elementsList = $(".carousel-list");
 
-    var pixelsOffset = 250;
+    var pixelsOffset = 225;
     var currentLeftValue = 0;
     var elementsCount = elementsList.find('li').length;
-    var minimumOffset = - ((elementsCount - 5) * pixelsOffset);
+    var elementsCountR=elementsCount-4;
+    var elementsCountL=0;
+    var minimumOffset = - ((elementsCount - 4) * pixelsOffset);
     var maximumOffset = 0;
 
     leftUIEl.click(function() {
         if (currentLeftValue != maximumOffset) {
-            currentLeftValue += 800;
-            elementsList.animate({ marginLeft : currentLeftValue + "px"}, 500);
+            if(elementsCountL<4){
+            currentLeftValue += pixelsOffset*elementsCountL;
+            elementsCountR+=elementsCountL;
+            elementsCountL=0;
+            }
+            else{
+            currentLeftValue += 900;
+            elementsCountL-=4;
+            elementsCountR+=4;
+            }
+            elementsList.animate({ marginLeft : currentLeftValue + "px"}, 700);
         }
     });
 
     rightUIEl.click(function() {
-        if (currentLeftValue != minimumOffset) {
-            currentLeftValue -= 800;
-            elementsList.animate({ marginLeft : currentLeftValue + "px"}, 500);
+        if (currentLeftValue > minimumOffset){
+            if(elementsCountR<4){
+            currentLeftValue -= pixelsOffset*elementsCountR;
+            elementsCountL+=elementsCountR;
+            elementsCountR=0;
+            }
+            else{
+            currentLeftValue -= 900;
+            elementsCountR-=4;
+            elementsCountL+=4;
+            }
+            elementsList.animate({ marginLeft : currentLeftValue + "px"}, 700);
         }
     });
+});
+$(function(){
+  $('.list-toggle').click(function(){
+    $('.u-list').toggleClass('open');
+  });
 });
